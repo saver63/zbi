ab0437bf203446beb333882ec7caa3f32dd40d3f
laozhao <shenghuangji@gmail.com>
2024-02-05 21:51:01 +0800

    first repo

---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.springbootinit.model.entity.Chart;
+
+/**
+* @author Administrator
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2024-02-04 20:35:27
+*/
+public interface ChartService extends IService<Chart> {
+
+}
